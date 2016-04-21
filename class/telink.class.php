6add5a22725eb809e0aa75adffae7c1a6c5d486c
root <root@srv8.atm-consulting.fr>
2016-04-21 10:40:33 +0200

    FIX ma société de la commande client doit être la société correspondant à l'entité d'origine

--- class/telink.class.php
+++ class/telink.class.php
@@ -26,13 +26,14 @@
 		}
 		
 		static function cloneOrder($idOrderSource, $toEntity) {
-		global $db,$conf, $user;
+		global $db,$conf, $user, $mc;
 		
 			$cf=new CommandeFournisseur($db);
 			$cf->fetch($idOrderSource);
 			
 			
-			$res = $db->query("SELECT fk_soc FROM ".MAIN_DB_PREFIX."thirdparty_entity WHERE fk_entity=".$toEntity." AND entity=".$conf->entity );	
+			//$res = $db->query("SELECT fk_soc FROM ".MAIN_DB_PREFIX."thirdparty_entity WHERE fk_entity=".$toEntity." AND entity=".$conf->entity );	
+			$res = $db->query("SELECT fk_soc FROM ".MAIN_DB_PREFIX."thirdparty_entity WHERE entity=".$toEntity." AND fk_entity=".$conf->entity ); //Attention, cela permet de créer la commande sur la société correspondant à l'entité emettrice
 			$obj = $db->fetch_object($res);	
 				
 			if($obj->fk_soc>0) {
@@ -41,16 +42,16 @@
 				
 				$res2 = $db->query("SELECT rowid FROM ".MAIN_DB_PREFIX."commande
 						 WHERE entity=".$toEntity." AND ref_client='".$cf->ref."'" );	
+
 				$obj2 = $db->fetch_object($res2);	
 				if($obj2->rowid>0) {
-					// la facture existe déjà dans le système en face. On la supprime
+					// la facture commande déjà dans le système en face. On la supprime
 					$o=new Commande($db);
 					
 					$previous_entity = $conf->entity;
 					$conf->entity = $toEntity;
 					if($o->fetch($obj2->rowid)>0) {
 						$o->delete($user);
-						
 						
 					}
 					
@@ -63,7 +64,10 @@
 				$o->ref_client = $cf->ref;
 				$o->socid = $obj->fk_soc;
 				$o->lines = $cf->lines;
-				
+
+				$o->fk_project = $cf->fk_project; //TODO check if it's shared project
+
+//var_dump($cf,$o);exit;
 				if($o->create($user)<0) {
 					
 					var_dump($o);
